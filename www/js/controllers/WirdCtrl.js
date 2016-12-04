f7a56cd06c5e516591686dcaaef2ed3f1b6be84c
Obada Kadri <brobada@gmail.com>
2016-12-03 20:19:40 -0800

    Paged wird with keep screen awake

--- www/js/controllers/WirdCtrl.js
+++ www/js/controllers/WirdCtrl.js
@@ -1,22 +1,32 @@
-app.controller('WirdCtrl', ['$scope', '$stateParams', '$ionicHistory', 'actionCreators',
-	function ($scope, $stateParams, $ionicHistory, actionCreators) {
+app.controller('WirdCtrl', ['$scope', '$stateParams', '$ionicHistory', '$window', 'actionCreators',
+	function($scope, $stateParams, $ionicHistory, $window, actionCreators) {
 		$scope.wirdId = parseInt($stateParams.wirdId);
 		$scope.wird = {};
 		$scope.imgRoot = "";
+
+		if ($window.plugins) {
+			$window.plugins.insomnia.keepAwake();
+		}
 
 		if (typeof cordova !== 'undefined') {
 			$scope.imgRoot = cordova.file.externalRootDirectory + "hifzTracker/width_800";
 		}
 
 		if ($scope.wirdId) {
-			$scope.wird = allWirds.find(function(wird){ return wird.id == $scope.wirdId; });
+			$scope.wird = allWirds.find(function(wird) { return wird.id == $scope.wirdId; });
 			$scope.activePage = $scope.wird.endPage - $scope.wird.startPage + 1;
 			$scope.wirdPages = new Array($scope.wird.endPage - $scope.wird.startPage + 3);
 		}
 
-		$scope.goBack = function () {
+		$scope.goBack = function() {
 			$ionicHistory.goBack();
 		};
+
+		$scope.$on("$destroy", function() {
+			if ($window.plugins) {
+				$window.plugins.insomnia.allowSleepAgain();
+			}
+		});
 
 	}]);
 
