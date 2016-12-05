faf7c5c465c70fc4f876f8362eae2d9565e31afb
Obada Kadri <brobada@gmail.com>
2016-12-04 21:48:53 -0800

    Fixes #29

--- www/js/controllers/HomeCtrl.js
+++ www/js/controllers/HomeCtrl.js
@@ -1,28 +1,35 @@
 app.controller('HomeCtrl', ['$scope', '$rootScope', '$ionicPopover', '$ionicPopup', '$state', 'stateService', 'actionCreators',
-	function($scope, $rootScope, $ionicPopover, $ionicPopup, $state, stateService, actionCreators) {
+	function ($scope, $rootScope, $ionicPopover, $ionicPopup, $state, stateService, actionCreators) {
 
-		$scope.view = {
-			state: stateService.getState()
+		var getStats = function () {
+			try {
+				return $scope.view.state.users
+					.list[$scope.view.state.ui.currentId].getStats();
+			} catch (e) {
+				return [];
+			}
 		};
 
-		$scope.restore = function() {
+		$scope.view = {
+			state: stateService.getState(),
+			userStats: getStats()
+		};
+
+		$scope.restore = function () {
 			actionCreators.restore($scope.view.backupJson);
 		};
 
-		$rootScope.$on('stateChanged', function(event, data) {
+		$rootScope.$on('stateChanged', function (event, data) {
 			$scope.view.state = data.state;
-			try {
-				$scope.view.userStats = $scope.view.state.users
-					.list[$scope.view.state.ui.currentId].getStats();
-			} catch (e) {}
+			$scope.view.userStats = getStats();
 		});
 
-		$scope.rateWird = function(index, rating) {
+		$scope.rateWird = function (index, rating) {
 			var user = $scope.view.state.users.list[$scope.view.state.ui.currentId];
 			actionCreators.rateWird(index, rating, user);
 		};
 
-		$scope.removeWird = function(index) {
+		$scope.removeWird = function (index) {
 			var user = $scope.view.state.users.list[$scope.view.state.ui.currentId];
 			$scope.surah = user.wirds[index];
 			var confirmPopup = $ionicPopup.confirm({
@@ -30,7 +37,7 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$ionicPopover', '$ionicPopu
 				template: '<span translate="DELETE_CONFIRMATION"></span> <b translate="{{surah.title}}"></b>?'
 			});
 
-			confirmPopup.then(function(res) {
+			confirmPopup.then(function (res) {
 				if (res) {
 					actionCreators.removeWird(index, user);
 				}
@@ -40,28 +47,28 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$ionicPopover', '$ionicPopu
 		// Initialize Add-Wird popover
 		$ionicPopover.fromTemplateUrl('templates/add-wird.html', {
 			scope: $scope
-		}).then(function(popover) {
+		}).then(function (popover) {
 			$scope.popover = popover;
 			$scope.wirdTypes = ['SURAH', 'QUARTER'];
 			$scope.allWirds = allWirds;
 			$scope.view.wirdLimit = 20;
 		});
 		// Reset add-wird view when popover is hidden
-		$scope.$on('popover.hidden', function() {
+		$scope.$on('popover.hidden', function () {
 			$scope.view.wirdLimit = 20;
 			delete $scope.view.wirdType;
 		});
 		// Open the popover
-		$scope.addWirdDialog = function($event) {
+		$scope.addWirdDialog = function ($event) {
 			$scope.popover.show($event);
 		};
 		// Add-wird infinite scroll
-		$scope.increaseWirdLimit = function() {
+		$scope.increaseWirdLimit = function () {
 			$scope.view.wirdLimit += 20;
 			$scope.$broadcast('scroll.infiniteScrollComplete');
 		};
 
-		$scope.addWird = function(wird) {
+		$scope.addWird = function (wird) {
 			var user = $scope.view.state.users.list[$scope.view.state.ui.currentId];
 			actionCreators.addWird(wird, user);
 		};
@@ -70,7 +77,7 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$ionicPopover', '$ionicPopu
 			$state.go("app.wirds", { "wirdId": wirdId });
 		};
 
-		$scope.loadMore = function(increment) {
+		$scope.loadMore = function (increment) {
 			actionCreators.loadMore(increment);
 		};
 
